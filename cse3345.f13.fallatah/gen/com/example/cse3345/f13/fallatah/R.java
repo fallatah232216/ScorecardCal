d6a7e1b12d05b93a762167a8987e86d97d7a8c97
Khalid Fallatah <kfallatah@smu.edu>
2013-12-09 21:24:54 -0600

    Redo
    Redo the fighters names line

--- cse3345.f13.fallatah/gen/com/example/cse3345/f13/fallatah/R.java
+++ cse3345.f13.fallatah/gen/com/example/cse3345/f13/fallatah/R.java
@@ -24,22 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int fight_1=0x7f080000;
-        public static final int vs=0x7f080001;
+        public static final int action_settings=0x7f080001;
+        public static final int linearLayout1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity1=0x7f030000;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int VS=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int fighter1=0x7f050002;
-        public static final int fighter2=0x7f050003;
     }
     public static final class style {
         /** 
