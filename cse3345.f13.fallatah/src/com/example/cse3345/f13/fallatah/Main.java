f8805c59a27c45d67e1c4203a531a56b084c4cfe
Khalid Fallatah <kfallatah@smu.edu>
2013-12-10 17:00:36 -0600

    add menu
    add menu in app

--- cse3345.f13.fallatah/src/com/example/cse3345/f13/fallatah/Main.java
+++ cse3345.f13.fallatah/src/com/example/cse3345/f13/fallatah/Main.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -151,8 +152,8 @@ public class Main extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.main, menu);
         return true;
     }
     
