1a164f0edfcd8ea67b61a43f4956399077ba0c51
Khalid Fallatah <kfallatah@smu.edu>
2013-12-10 14:17:34 -0600

    the radio buttons
    create

--- cse3345.f13.fallatah/gen/com/example/cse3345/f13/fallatah/R.java
+++ cse3345.f13.fallatah/gen/com/example/cse3345/f13/fallatah/R.java
@@ -24,12 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f08000a;
         public static final int editText1=0x7f080001;
         public static final int editText2=0x7f080003;
         public static final int linearLayout1=0x7f080000;
+        public static final int radio=0x7f080006;
+        public static final int radioButton1=0x7f080007;
+        public static final int radioButton2=0x7f080008;
         public static final int textView1=0x7f080002;
         public static final int textView2=0x7f080004;
+        public static final int textView3=0x7f080009;
+        public static final int view1=0x7f080005;
     }
     public static final class layout {
         public static final int activity1=0x7f030000;
@@ -38,7 +43,9 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Amature=0x7f050004;
         public static final int Competition=0x7f050003;
+        public static final int Professional=0x7f050005;
         public static final int VS=0x7f050002;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
