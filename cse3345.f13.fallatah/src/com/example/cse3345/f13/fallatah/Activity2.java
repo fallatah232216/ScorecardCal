f1a419b63ca9086fb206893e095308f373fb03d4
Khalid Fallatah <kfallatah@smu.edu>
2013-12-11 13:13:17 -0600

    variable
    declaring

--- cse3345.f13.fallatah/src/com/example/cse3345/f13/fallatah/Activity2.java
+++ cse3345.f13.fallatah/src/com/example/cse3345/f13/fallatah/Activity2.java
@@ -1,10 +1,17 @@
 package com.example.cse3345.f13.fallatah;
 
+
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class Activity2 extends Activity {
+	
+	//declaring the variable static variable
+			public static String winner;
+			String roundno=String.valueOf(Main.rounds.getSelectedItem());
+			int firsttotal,secondtotal;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
