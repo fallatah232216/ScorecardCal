874daff0b507a4e3ff4977d879948f314e2dea34
Khalid Fallatah <kfallatah@smu.edu>
2013-12-10 20:50:32 -0600

    creating activities

--- cse3345.f13.fallatah/gen/com/example/cse3345/f13/fallatah/R.java
+++ cse3345.f13.fallatah/gen/com/example/cse3345/f13/fallatah/R.java
@@ -28,11 +28,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090036;
+        public static final int action_settings=0x7f090037;
         public static final int button1=0x7f09000d;
         public static final int editText1=0x7f090001;
         public static final int editText2=0x7f090003;
         public static final int linearLayout1=0x7f090000;
+        public static final int mail=0x7f090036;
         public static final int radio=0x7f090006;
         public static final int radioButton1=0x7f090007;
         public static final int radioButton2=0x7f090008;
@@ -87,16 +88,25 @@ public final class R {
     public static final class layout {
         public static final int activity1=0x7f030000;
         public static final int activity2=0x7f030001;
+        public static final int activity3=0x7f030002;
+        public static final int menu=0x7f030003;
+        public static final int round10=0x7f030004;
+        public static final int round3=0x7f030005;
+        public static final int round4=0x7f030006;
+        public static final int round6=0x7f030007;
+        public static final int round8=0x7f030008;
     }
     public static final class menu {
         public static final int activity2=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int activity3=0x7f080001;
+        public static final int main=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050003;
         public static final int title_activity_activity2=0x7f050002;
+        public static final int title_activity_activity3=0x7f050004;
     }
     public static final class style {
         /** 
